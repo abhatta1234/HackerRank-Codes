3e942eaf424bfa9d705170cdf20f565baf7b0c39
abhatta1234 <40056517+abhatta1234@users.noreply.github.com>
2019-05-29 10:12:01 -0500

    Add files via upload

---
+++ test.py
@@ -0,0 +1,32 @@
+
+num = int(input())
+matrix =[]
+
+for i in range(num):
+    data = list(map(int,input().split()))
+    matrix.append(data)
+sum_d1 = 0
+sum_d2 = 0
+row = num - 1
+
+if (len(matrix[0])) != num :
+    print("The given Matrix is not Square")
+else:
+    for i in range(num):
+        sum_d1+= matrix[i][i]
+        sum_d2+= matrix[row][i]
+        row = row - 1
+
+print(abs(sum_d1-sum_d2))
+
+
+
+
+
+
+
+
+
+
+
+
