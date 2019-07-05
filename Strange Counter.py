4740d681185436d6dbd305f6c85491b93b149e0a
abhatta1234 <40056517+abhatta1234@users.noreply.github.com>
2019-07-05 12:54:01 -0500

    Add files via upload

--- Strange Counter.py
+++ Strange Counter.py
@@ -1,12 +1,19 @@
+# A strange counter displays 3 in the first second. Each second the number displayed decreases by 1 until it reaches 1.
+# Then, it starts displaying double of what it started the last cycle. Given any second find what is being displayed on
+# the counter.
+
 num = int(input())
 rep = 3
 count = 0
 a = 0
+
 while num> count:
     a+=1
     lastrec = count + 1
     count+= rep
     rep = rep*2
+
 start = 3*2**(a-1)
 distance = num - lastrec
+
 print(start - distance)
